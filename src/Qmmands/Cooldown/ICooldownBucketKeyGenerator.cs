8a0f5eb76a89ed3f0f82279b66d2df8cb1b4c62f
Quahu <quahuu@gmail.com>
2018-10-31 19:42:37 +0100

    [ci skip] Adjusted docs for ICooldownBucketKeyGenerator.
    Co-Authored-By: Allan <kiritsu@users.noreply.github.com>

--- src/Qmmands/Cooldown/ICooldownBucketKeyGenerator.cs
+++ src/Qmmands/Cooldown/ICooldownBucketKeyGenerator.cs
@@ -10,10 +10,12 @@ namespace Qmmands
         /// <summary>
         ///     Returns a <see cref="Cooldown"/> bucket key generated using the specified <paramref name="bucketType"/>.
         /// </summary>
-        /// <param name="bucketType"> The bucket type. </param>
+        /// <param name="bucketType"> The <see langword="enum"/> bucket type. </param>
         /// <param name="context"> The <see cref="ICommandContext"/> used for execution. </param>
         /// <param name="provider"> The <see cref="IServiceProvider"/> used for execution. </param>
-        /// <returns></returns>
+        /// <returns>
+        ///     The <see cref="Cooldown"/> bucket key.
+        /// </returns>
         object GenerateBucketKey(object bucketType, ICommandContext context, IServiceProvider provider);
     }
 }
