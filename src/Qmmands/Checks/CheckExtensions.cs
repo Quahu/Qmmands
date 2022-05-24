7e82a5881399c932b32a1b184568e2492e18ca51
Quahu <quahuu@gmail.com>
2022-05-24 19:57:13 +0200

    Moved check running logic to extension methods on the constructs.

---
+++ src/Qmmands/Checks/CheckExtensions.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+using Qommon.Collections;
+
+namespace Qmmands;
+
+[EditorBrowsable(EditorBrowsableState.Never)]
+public static class CheckExtensions
+{
+    private static Task<KeyValuePair<ICheck, IResult>>[] ChecksToTasks(ICommandContext context, IReadOnlyList<ICheck> checks)
+    {
+        static async Task<KeyValuePair<ICheck, IResult>> RunCheckAsync(ICommandContext context, ICheck check)
+        {
+            var checkResult = await check.CheckAsync(context).ConfigureAwait(false);
+            return KeyValuePair.Create(check, checkResult);
+        }
+
+        var checkCount = checks.Count;
+        var tasks = new Task<KeyValuePair<ICheck, IResult>>[checkCount];
+        for (var i = 0; i < checkCount; i++)
+        {
+            tasks[i] = RunCheckAsync(context, checks[i]);
+        }
+
+        return tasks;
+    }
+
+    public static async ValueTask<IResult> RunChecksAsync(this IModule module, ICommandContext context)
+    {
+        var parent = module.Parent;
+        if (parent != null)
+        {
+            var result = await parent.RunChecksAsync(context).ConfigureAwait(false);
+            if (!result.IsSuccessful)
+                return result;
+        }
+
+        if (module.Checks.Count > 0)
+        {
+            var checkResults = await Task.WhenAll(ChecksToTasks(context, module.Checks)).ConfigureAwait(false);
+            var failedGroups = checkResults.GroupBy(x => x.Key.Group)
+                .Where(x => x.Key == null ? x.Any(y => !y.Value.IsSuccessful) : x.All(y => !y.Value.IsSuccessful)).ToArray();
+
+            if (failedGroups.Length > 0)
+                return new ChecksFailedResult(module, failedGroups.SelectMany(x => x).Where(x => !x.Value.IsSuccessful).ToDictionary());
+        }
+
+        return Results.Success;
+    }
+
+    public static async ValueTask<IResult> RunChecksAsync(this ICommand command, ICommandContext context)
+    {
+        var result = await command.Module.RunChecksAsync(context).ConfigureAwait(false);
+        if (!result.IsSuccessful)
+            return result;
+
+        if (command.Checks.Count > 0)
+        {
+            var checkResults = await Task.WhenAll(ChecksToTasks(context, command.Checks)).ConfigureAwait(false);
+            var failedGroups = checkResults.GroupBy(x => x.Key.Group)
+                .Where(x => x.Key == null ? x.Any(y => !y.Value.IsSuccessful) : x.All(y => !y.Value.IsSuccessful)).ToArray();
+
+            if (failedGroups.Length > 0)
+                return new ChecksFailedResult(command, failedGroups.SelectMany(x => x).Where(x => !x.Value.IsSuccessful).ToDictionary());
+        }
+
+        return Results.Success;
+    }
+}
--- src/Qmmands/Default/ExecutionSteps/RunChecks.cs
+++ src/Qmmands/Default/ExecutionSteps/RunChecks.cs
@@ -1,8 +1,5 @@
-﻿using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using Qommon;
-using Qommon.Collections;
 
 namespace Qmmands.Default;
 
@@ -14,60 +11,10 @@ public static partial class DefaultExecutionSteps
         {
             Guard.IsNotNull(context.Command);
 
-            static Task<KeyValuePair<ICheck, IResult>>[] ChecksToTasks(ICommandContext context, IReadOnlyList<ICheck> checks)
-            {
-                static async Task<KeyValuePair<ICheck, IResult>> RunCheckAsync(ICommandContext context, ICheck check)
-                {
-                    var checkResult = await check.CheckAsync(context).ConfigureAwait(false);
-                    return KeyValuePair.Create(check, checkResult);
-                }
-
-                var checkCount = checks.Count;
-                var tasks = new Task<KeyValuePair<ICheck, IResult>>[checkCount];
-                for (var i = 0; i < checkCount; i++)
-                {
-                    tasks[i] = RunCheckAsync(context, checks[i]);
-                }
-
-                return tasks;
-            }
-
-            static async ValueTask<IResult> RunChecksAsync(ICommandContext context, IModule module)
-            {
-                if (module.Parent != null)
-                {
-                    var result = await RunChecksAsync(context, module.Parent).ConfigureAwait(false);
-                    if (!result.IsSuccessful)
-                        return result;
-                }
-
-                if (module.Checks.Count > 0)
-                {
-                    var checkResults = await Task.WhenAll(ChecksToTasks(context, module.Checks)).ConfigureAwait(false);
-                    var failedGroups = checkResults.GroupBy(x => x.Key.Group)
-                        .Where(x => x.Key == null ? x.Any(y => !y.Value.IsSuccessful) : x.All(y => !y.Value.IsSuccessful)).ToArray();
-
-                    if (failedGroups.Length > 0)
-                        return new ChecksFailedResult(module, failedGroups.SelectMany(x => x).Where(x => !x.Value.IsSuccessful).ToDictionary());
-                }
-
-                return Results.Success;
-            }
-
             var command = context.Command;
-            var result = await RunChecksAsync(context, command.Module).ConfigureAwait(false);
+            var result = await command.RunChecksAsync(context).ConfigureAwait(false);
             if (!result.IsSuccessful)
                 return result;
-
-            if (command.Checks.Count > 0)
-            {
-                var checkResults = await Task.WhenAll(ChecksToTasks(context, command.Checks)).ConfigureAwait(false);
-                var failedGroups = checkResults.GroupBy(x => x.Key.Group)
-                    .Where(x => x.Key == null ? x.Any(y => !y.Value.IsSuccessful) : x.All(y => !y.Value.IsSuccessful)).ToArray();
-
-                if (failedGroups.Length > 0)
-                    return new ChecksFailedResult(command, failedGroups.SelectMany(x => x).Where(x => !x.Value.IsSuccessful).ToDictionary());
-            }
 
             return await Next.ExecuteAsync(context);
         }
