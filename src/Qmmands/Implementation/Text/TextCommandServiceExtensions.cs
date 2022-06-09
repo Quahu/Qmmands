af8a75bb51a798b12f91476755a285a09b32c795
Quahu <quahuu@gmail.com>
2022-06-09 13:15:40 +0200

    Added ICommandService.EnumerateTextModules().

---
+++ src/Qmmands/Implementation/Text/TextCommandServiceExtensions.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace Qmmands.Text;
+
+/// <summary>
+///     Represents text extensions for <see cref="ICommandService"/>.
+/// </summary>
+[EditorBrowsable(EditorBrowsableState.Never)]
+public static class TextCommandServiceExtensions
+{
+    /// <summary>
+    ///     Enumerates top-level text modules added to this command service.
+    /// </summary>
+    /// <param name="commandService"> This command service. </param>
+    /// <returns>
+    ///     An enumerable of the modules.
+    /// </returns>
+    public static IEnumerable<ITextModule> EnumerateTextModules(this ICommandService commandService)
+    {
+        static IEnumerable<ITextModule> YieldModules(IEnumerable<IModule> modules)
+        {
+            foreach (var module in modules)
+            {
+                yield return (ITextModule) module;
+            }
+        }
+
+        foreach (var kvp in commandService.EnumerateModules())
+        {
+            if (!typeof(ITextCommandMap).IsAssignableFrom(kvp.Key))
+                continue;
+
+            return YieldModules(kvp.Value);
+        }
+
+        return Array.Empty<ITextModule>();
+    }
+}
